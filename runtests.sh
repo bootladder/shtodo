3b36416d94e0a70729b32ee61fda9012ac81144d
steve <steve@bootladder.com>
2018-10-01 16:57:48 -0700

    rename hello.bats to test_shtodo.bats and call it in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,7 @@ echo running.... go test
 go test
 echo running.... go build.....
 go build
-docker run -t --rm --volume=$(pwd):/tmp hello /tmp/bats/bin/bats /tmp/tests/hello.bats
-docker run -t --rm --volume=$(pwd):/tmp hello /bin/bash -c "cd /tmp && bundle exec cucumber"
-docker run -t --rm --volume=$(pwd):/tmp hello echo hello
+docker run -t --rm --volume=$(pwd):/tmp hello /tmp/bats/bin/bats /tmp/tests/test_shtodo.bats
+#docker run -t --rm --volume=$(pwd):/tmp hello /bin/bash -c "cd /tmp && bundle exec cucumber"
+#docker run -t --rm --volume=$(pwd):/tmp hello echo hello
 rm shtodo
