ba1163b5a91aedd966323d1c8bf73a76022ef589
Steve Anderson <steve@integral.io>
2018-10-03 15:47:09 -0400

    runtests.sh in a loop, nicer output

--- runtests.sh
+++ runtests.sh
@@ -7,9 +7,17 @@ go test
 echo ========= ACCEPTANCE TESTS ================
 echo running.... go build.....
 go build
-docker run -t --rm --volume=$(pwd):/opt hello /opt/bats/bin/bats /opt/tests/test_shtodo1.bats
-docker run -t --rm --volume=$(pwd):/opt hello /opt/bats/bin/bats /opt/tests/test_shtodo2.bats
-docker run -t --rm --volume=$(pwd):/opt hello /opt/bats/bin/bats /opt/tests/test_shtodo3.bats
+
+echo running.... test loop.....
+echo
+for TEST_FILE in $(cd tests; ls test_*.bats)
+do
+  echo ==== $TEST_FILE ====
+  docker run -t --rm --volume=$(pwd):/opt hello /opt/bats/bin/bats /opt/tests/$TEST_FILE 
+  echo 
+done
+
+
 #docker run -t --rm --volume=$(pwd):/tmp hello /bin/bash -c "cd /tmp && bundle exec cucumber"
 #docker run -t --rm --volume=$(pwd):/opt hello ls -al /tmp/
 rm shtodo
