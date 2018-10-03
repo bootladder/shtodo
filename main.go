238eb7d13b5107e60720f90218420728b9a8a7cd
steve <steve@bootladder.com>
2018-10-03 16:00:29 -0700

    move topflow into main, because it isn't unit tested

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "io/ioutil"
+    "fmt"
+    "time"
 )
 
 var external = External{}
@@ -17,4 +19,25 @@ func main() {
 
     inject()
     topflow()
+}
+
+func topflow() {
+
+    var myConfig = &Config{}
+    var err = myConfig.ParseConfigFile("/etc/shtodo.conf")
+    Fatal(err)
+
+    var path = myConfig.GetPathToTodo()
+
+    var todoContents string = ReadTodo(path)
+
+    TouchLastTimeFile()
+    var tbefore time.Time = ReadLastPrintedTodoTime(pathtolasttime)
+
+    var tnow time.Time = time.Now().UTC()
+
+    if ShouldPrint(tnow,tbefore,30) {
+      fmt.Print(todoContents)
+      UpdateLastTimeFile(tnow)
+    }
 }
